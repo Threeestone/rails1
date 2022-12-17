a36de2850dbb92c87fd69a1e0e1b2ad4f6ecba3f
Threeestone <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2022-12-17 11:44:13 +0000

    [finish]2-4 git commit -m [finish]2-

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
- 
+  # matching to ../lists/1 and ../lists/3
+  get 'lists/:id' => 'lists#show', as: 'list'
+
 end
