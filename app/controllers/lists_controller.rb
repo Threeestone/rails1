cc3f18ffa52ebd13c1463f4737c28352f0ffdc1b
Threeestone <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2022-12-20 13:37:54 +0000

    [finish]3-6~10,[start]3-11

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,9 +4,14 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+     redirect_to list_path(@list.id)
+       params.permit(:image)
+    else
+      @lists = List.all
+      render :index
+    end
   end
 
   def index
@@ -18,11 +23,24 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to　list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
 
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :images)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  # matching to ../lists/1 and ../lists/3
-  get 'lists/:id' => 'lists#show', as: 'list'
+
+ get 'top' => 'homes#top'
+ resources :lists
 
 end
+
